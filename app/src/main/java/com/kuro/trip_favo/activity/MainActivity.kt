d9cebd3624f12bf3a4e72b2e6ab8c84dd549f5b7
ayu <96kawa.0711@gmail.com>
2022-01-28 16:25:25 +0900

    BottomNavigationViewを表示、ActionBarからToolBarに変更

--- app/src/main/java/com/kuro/trip_favo/activity/MainActivity.kt
+++ app/src/main/java/com/kuro/trip_favo/activity/MainActivity.kt
@@ -2,14 +2,19 @@ package com.kuro.trip_favo.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.kuro.trip_favo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val topToolbar = findViewById<Toolbar>(R.id.top_toolbar)
+        setSupportActionBar(topToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
     }
 
 
 
-}
