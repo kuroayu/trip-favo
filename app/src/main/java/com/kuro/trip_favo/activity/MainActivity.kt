4c0c1474fe2d7a15eb45642b320843b951cd2ded
ayu <96kawa.0711@gmail.com>
2022-01-28 15:40:32 +0900

    no message

--- app/src/main/java/com/kuro/trip_favo/activity/MainActivity.kt
+++ app/src/main/java/com/kuro/trip_favo/activity/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.kuro.trip_favo
+package com.kuro.trip_favo.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kuro.trip_favo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
 }
