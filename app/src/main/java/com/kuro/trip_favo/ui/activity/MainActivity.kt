27edd41a4337650b75416c0332547f663ae4f714
ayu <96kawa.0711@gmail.com>
2022-02-02 08:14:01 +0900

    BottomNavi

---
+++ app/src/main/java/com/kuro/trip_favo/ui/activity/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.kuro.trip_favo.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kuro.trip_favo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val topToolbar = findViewById<Toolbar>(R.id.top_toolbar)
+        setSupportActionBar(topToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        setupWithNavController(bottomNavigation, navController)
+
+
+    }
+    }
+
+
+
---
+++ app/src/main/java/com/kuro/trip_favo/ui/fragment/SearchFragment.kt
@@ -0,0 +1,20 @@
+package com.kuro.trip_favo.ui.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kuro.trip_favo.R
+
+class SearchFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.activity_search_fragment, container, false)
+    }
+}
