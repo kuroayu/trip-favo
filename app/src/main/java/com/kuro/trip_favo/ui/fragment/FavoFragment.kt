c61bf21b65b5c1f39e584c6c5b981b9d019a915b
ayu <96kawa.0711@gmail.com>
2022-02-11 14:29:37 +0900

    デモデータ設定、UI確認

--- app/src/main/java/com/kuro/trip_favo/ui/fragment/FavoFragment.kt
+++ app/src/main/java/com/kuro/trip_favo/ui/fragment/FavoFragment.kt
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.kuro.trip_favo.R
+import com.kuro.trip_favo.ui.DummyData
+import com.kuro.trip_favo.ui.FavoAdapter
 
 class FavoFragment : Fragment() {
 
@@ -16,12 +21,33 @@ class FavoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.activity_favofragment, container, false)
+
+        val view = inflater.inflate(R.layout.activity_favofragment, container, false)
+        val linearLayoutManager = LinearLayoutManager(view.context)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.fovo_recyclerview)
+        val adapter = FavoAdapter(dummyLists())
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.addItemDecoration(DividerItemDecoration(view.context, LinearLayoutManager.VERTICAL))
+
+        return view
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Log.d("fragment","favofragment onCreate")
+    }
+
+    private fun dummyLists(): MutableList<DummyData> {
+        var dummyLists: MutableList<DummyData> = ArrayList()
+        var dummy = DummyData("アパホテル",24000,"石川県金沢市2")
+        var i = 0
+        while (i < 20){
+            i++
+            dummyLists.add(dummy)
+        }
+
+     return dummyLists
     }
 }
