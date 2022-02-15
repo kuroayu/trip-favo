97f406407897bcf76e608e15edf3bfcd2d2bc0a0
ayu <96kawa.0711@gmail.com>
2022-02-15 18:19:29 +0900

    ・お気に入り画面内検索機能のデザイン ・お気に入り画面FABボタン設置 ・さがす画面のデザイン ・さがす画面からの検索結果を表示する画面のデザイン

---
+++ app/src/main/java/com/kuro/trip_favo/ui/fragment/SearchResultFragment.kt
@@ -0,0 +1,12 @@
+package com.kuro.trip_favo.ui.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kuro.trip_favo.R
+
+class SearchResultFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result_fragment)
+    }
+}
